36aa64c731478c7ae38c938b7f3294d03c567455
nick-perry14 <6935777+nick-perry14@users.noreply.github.com>
2021-04-03 15:08:52 -0400

    Added Events package and Events Loader

---
+++ src/me/nick_perry14/Bukkit_ULX/events/EventLoader.java
@@ -0,0 +1,13 @@
+package me.nick_perry14.Bukkit_ULX.events;
+
+import me.nick_perry14.Bukkit_ULX.Main;
+
+public final class EventLoader {
+	private EventLoader() {
+		
+	}
+	
+	public static void loadEvents(Main instance) {
+		
+	}
+}
