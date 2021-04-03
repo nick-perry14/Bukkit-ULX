c7d2ab142d09788e3e1d538f07de3fd44051b26a
nick-perry14 <6935777+nick-perry14@users.noreply.github.com>
2021-04-03 15:19:42 -0400

    Added EventLoader Method

--- src/me/nick_perry14/Bukkit_ULX/events/EventLoader.java
+++ src/me/nick_perry14/Bukkit_ULX/events/EventLoader.java
@@ -1,13 +1,17 @@
 package me.nick_perry14.Bukkit_ULX.events;
 
 import me.nick_perry14.Bukkit_ULX.Main;
+import me.nick_perry14.Bukkit_ULX.events.connection.PlayerLeave;
 
 public final class EventLoader {
 	private EventLoader() {
 		
 	}
-	
+	/**
+	 * Method to load all event listeners
+	 * @param instance JavaPlugin Instance
+	 */
 	public static void loadEvents(Main instance) {
-		
+		instance.getPluginLoader().createRegisteredListeners(new PlayerLeave(), instance);
 	}
 }
